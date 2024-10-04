0fa560ca1e8749af9911cd45968bebbcac429467
Aung Ye Htet <aung.atg1738@gmail.com>
2024-10-04 16:37:25 +0630

    points and sales Person

--- app/helper/pointHelper.js
+++ app/helper/pointHelper.js
@@ -1,13 +1,13 @@
-"use strict"
-const fixedDiscountPercent = require("../models/fixedDiscountPercent")
-const giftPointRule = require("../models/giftPointRule")
-const patient = require("../models/patient")
-const pointDiscount = require("../models/pointDiscount")
-const pointRule = require("../models/pointRule")
-const tierLevel = require("../models/tierLevel")
-const { return404Response } = require("./404ErrorHelper")
+"use strict";
+const fixedDiscountPercent = require("../models/fixedDiscountPercent");
+const giftPointRule = require("../models/giftPointRule");
+const patient = require("../models/patient");
+const pointDiscount = require("../models/pointDiscount");
+const pointRule = require("../models/pointRule");
+const tierLevel = require("../models/tierLevel");
+const { return404Response } = require("./404ErrorHelper");
 
-//query Point Rule 
+//query Point Rule
 // async function getPointDiscountRule(req,res){
 //     let { treatmentType } = req.body
 //     const pointData = await pointDiscount.find({type: treatmentType}).sort({from: -1, to: -1})
@@ -17,53 +17,89 @@ const { return404Response } = require("./404ErrorHelper")
 //     return pointData
 // }
 
-
 //query Tier Level And Calculate Discount
-async function getPointDiscountRule(data){
-    let gift_Rule 
-    let { discountId, totalAmount, treatmentType, relatedGiftPointRules, tsType, msTotalAmount } = data
-    const { point, amount } = await pointRule.findOne({type: treatmentType})
-    relatedGiftPointRules ? gift_Rule = await giftPointRule.findById(relatedGiftPointRules) : gift_Rule = { point: 0 }
-    const giftPoint = gift_Rule.point
-    if(tsType === "MS"){
-        const pointAmount = ( Number(msTotalAmount) * Number(point)/ Number(amount) ) + giftPoint
-        return { point: pointAmount, gift_point: giftPoint }
-    }
-    const { percent } = await fixedDiscountPercent.findById(discountId)
-    const pointAmount = ( Number(totalAmount) * Number(point)/ Number(amount) ) + giftPoint
-    const discountAmount = Number(totalAmount) - ( Number(totalAmount) * Number(percent) / 100 )
-    const realAmount = Number(totalAmount)
-    return { point: pointAmount, discountTotalAmount: discountAmount, realTotalAmount: realAmount, percent: percent, gift_point: giftPoint }
+async function getPointDiscountRule(data) {
+  let gift_Rule;
+
+  let {
+    discountId,
+    totalAmount,
+    treatmentType,
+    relatedGiftPointRules,
+    tsType,
+    msTotalAmount,
+    relatedExtraPoint,
+  } = data;
+
+  const { point, amount } = await pointRule.findOne({ type: treatmentType });
+
+  relatedGiftPointRules
+    ? (gift_Rule = await giftPointRule.findById(relatedGiftPointRules))
+    : (gift_Rule = { point: 0 });
+
+  const giftPoint = gift_Rule.point;
+
+  if (tsType === "MS") {
+    const pointAmount =
+      (Number(msTotalAmount) * Number(point)) / Number(amount) +
+      giftPoint +
+      relatedExtraPoint;
+    return { point: pointAmount, gift_point: giftPoint };
+  }
+
+  const { percent } = await fixedDiscountPercent.findById(discountId);
+
+  const pointAmount =
+    (Number(totalAmount) * Number(point)) / Number(amount) +
+    giftPoint +
+    relatedExtraPoint;
+
+  const discountAmount =
+    Number(totalAmount) - (Number(totalAmount) * Number(percent)) / 100;
+
+  const realAmount = Number(totalAmount);
+
+  return {
+    point: pointAmount,
+    discountTotalAmount: discountAmount,
+    realTotalAmount: realAmount,
+    percent: percent,
+    gift_point: giftPoint,
+    extraPoint: relatedExtraPoint,
+  };
 }
 
 async function getPointRule(treatmentType) {
-    const pointData = await pointRule.find({type: treatmentType}).sort({amount: -1})
-    return pointData[0]
+  const pointData = await pointRule
+    .find({ type: treatmentType })
+    .sort({ amount: -1 });
+  return pointData[0];
 }
 
 exports.calculateDiscountByTotalAmount = (pointData, total, percent) => {
-    const totalAmount = Number(total)
-    for(let i = 0; i < pointData.length; i++) {
-        const nextIndex = i + 1
-        const firstAmount = pointData[i].from
-        const secondAmount = pointData[i].to
-        console.log("fir",firstAmount, secondAmount)
-        if(percent){
-            const pointRule = [ 30, 50 ]
-            const percentFromArray = pointRule.filter(po => po === Number(percent))[0] || 0
-            return { percent: percentFromArray, point: 1000 }
-        }
-        else if (firstAmount === secondAmount && totalAmount >= firstAmount){
-            return pointData[i]
-        }
-        else if(firstAmount === totalAmount || (totalAmount > firstAmount && totalAmount < secondAmount)) {
-            return pointData[i]
-        }
-        else if(nextIndex === pointData.length){
-            return { from: 0, to: 0, percent: 0 }
-        }
+  const totalAmount = Number(total);
+  for (let i = 0; i < pointData.length; i++) {
+    const nextIndex = i + 1;
+    const firstAmount = pointData[i].from;
+    const secondAmount = pointData[i].to;
+    console.log("fir", firstAmount, secondAmount);
+    if (percent) {
+      const pointRule = [30, 50];
+      const percentFromArray =
+        pointRule.filter((po) => po === Number(percent))[0] || 0;
+      return { percent: percentFromArray, point: 1000 };
+    } else if (firstAmount === secondAmount && totalAmount >= firstAmount) {
+      return pointData[i];
+    } else if (
+      firstAmount === totalAmount ||
+      (totalAmount > firstAmount && totalAmount < secondAmount)
+    ) {
+      return pointData[i];
+    } else if (nextIndex === pointData.length) {
+      return { from: 0, to: 0, percent: 0 };
     }
-}
+  }
+};
 
 // exports.PointData = {
 
@@ -73,45 +109,84 @@ exports.calculateDiscountByTotalAmount = (pointData, total, percent) => {
 //     maximum_point: null
 // }
 
+exports.CalculateWithExtraPoint = async (id, extraPoint) => {
+  await patient.findByIdAndUpdate(id, { $inc: { total_point: extraPoint } });
+};
+
 exports.AddPointByInput = async (id, totalPoint) => {
-    await patient.findByIdAndUpdate(id,{$inc: {total_point: totalPoint}})
-}
+  await patient.findByIdAndUpdate(id, { $inc: { total_point: totalPoint } });
+};
 
 exports.checkWithTierRule = async (totalPoint) => {
-    let id
-    const tierRule = await tierLevel.find().sort({total_point: -1})
-    for(let i = 0; i < tierRule.length; i++) {
-        if(totalPoint >= tierRule[i].total_point){
-            id = tierRule[i]._id
-            break
-        }
+  let id;
+  const tierRule = await tierLevel.find().sort({ total_point: -1 });
+  for (let i = 0; i < tierRule.length; i++) {
+    if (totalPoint >= tierRule[i].total_point) {
+      id = tierRule[i]._id;
+      break;
     }
-    return id
-}
+  }
+  return id;
+};
 
 exports.checkAndUpdateTierOfPatient = async (id) => {
-    const patientData = await patient.findById(id)
-    const tierId = await this.checkWithTierRule(patientData.total_point)
-    await patient.findByIdAndUpdate(id, {$set: {tierLevel: tierId}})
-}
+  const patientData = await patient.findById(id);
+  const tierId = await this.checkWithTierRule(patientData.total_point);
+  await patient.findByIdAndUpdate(id, { $set: { tierLevel: tierId } });
+};
 
-exports.CalculateAndPointToUserWithPointRule = async (req,res) => {
-    let { totalAmount, percentFromInput } = req.body
-    const pointData = await getPointDiscountRule(req,res)
-    let result = this.calculateDiscountByTotalAmount(pointData, totalAmount, percentFromInput)
-    let addPoint = result.point
-    await patient.findByIdAndUpdate(req.body.relatedPatient,{$inc: { total_point: addPoint }})
-    return addPoint
-}
+exports.CalculateAndPointToUserWithPointRule = async (req, res) => {
+  let { totalAmount, percentFromInput } = req.body;
+  const pointData = await getPointDiscountRule(req, res);
+  let result = this.calculateDiscountByTotalAmount(
+    pointData,
+    totalAmount,
+    percentFromInput
+  );
+  let addPoint = result.point;
+  await patient.findByIdAndUpdate(req.body.relatedPatient, {
+    $inc: { total_point: addPoint },
+  });
+  return addPoint;
+};
 
-exports.CalculateTotalAmountByPercent = async (req,res) => {
-    let pointData 
-    const { relatedPatient, totalAmount, treatmentType, relatedGiftPointRules, tsType, msTotalAmount } = req.body
-    const patientData = await patient.findOne({ _id: relatedPatient, walletUser: true}).populate("tierLevel")
-    if(!patientData) return { success: false, message: "Current User is not a wallet user." }
-    tsType === "TSMulti" ? pointData = await getPointDiscountRule({discountId: patientData.tierLevel.relatedFixedDiscountPercent, totalAmount: totalAmount, treatmentType: treatmentType, relatedGiftPointRules: relatedGiftPointRules}) : pointData = await getPointDiscountRule({tsType: tsType, treatmentType: treatmentType, relatedGiftPointRules: relatedGiftPointRules, msTotalAmount: msTotalAmount})
-    return { success: true, ...pointData }
-}
+exports.CalculateTotalAmountByPercent = async (req, res) => {
+  let pointData;
+  const {
+    relatedPatient,
+    totalAmount,
+    treatmentType,
+    relatedGiftPointRules,
+    tsType,
+    msTotalAmount,
+    relatedExtraPoint,
+  } = req.body;
+
+  const patientData = await patient
+    .findOne({ _id: relatedPatient, walletUser: true })
+    .populate("tierLevel");
+
+  if (!patientData)
+    return { success: false, message: "Current User is not a wallet user." };
+
+  tsType === "TSMulti"
+    ? (pointData = await getPointDiscountRule({
+        discountId: patientData.tierLevel.relatedFixedDiscountPercent,
+        totalAmount: totalAmount,
+        treatmentType: treatmentType,
+        relatedGiftPointRules: relatedGiftPointRules,
+        relatedExtraPoint: relatedExtraPoint,
+      }))
+    : (pointData = await getPointDiscountRule({
+        tsType: tsType,
+        treatmentType: treatmentType,
+        relatedGiftPointRules: relatedGiftPointRules,
+        msTotalAmount: msTotalAmount,
+        relatedExtraPoint: relatedExtraPoint,
+      }));
+
+  return { success: true, ...pointData };
+};
 
 //previous point rule with amount
 // exports.CalculateTotalAmountByPercent = async (req,res) => {
