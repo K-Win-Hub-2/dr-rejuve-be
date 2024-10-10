eb8f37ec11c94f6b812314a9a3fb46cf6578c6a5
Aung Ye Htet <aung.atg1738@gmail.com>
2024-10-10 17:04:41 +0630

    fix

--- app/models/tierLevel.js
+++ app/models/tierLevel.js
@@ -1,28 +1,28 @@
-"use strict";
+// "use strict";
 
-const mongoose = require("mongoose");
-mongoose.promise = global.Promise;
-const Schema = mongoose.Schema;
+// const mongoose = require("mongoose");
+// mongoose.promise = global.Promise;
+// const Schema = mongoose.Schema;
 
-let TierSchema = new Schema({
-  name: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  relatedFixedDiscountPercent: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "FixedDiscountPercents",
-  },
-  total_point: {
-    type: Number,
-    default: 0,
-  },
-  isDeleted: {
-    type: Boolean,
-    default: false,
-  },
-});
+// let TierSchema = new Schema({
+//   name: {
+//     type: String,
+//   },
+//   description: {
+//     type: String,
+//   },
+//   relatedFixedDiscountPercent: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: "FixedDiscountPercents",
+//   },
+//   total_point: {
+//     type: Number,
+//     default: 0,
+//   },
+//   isDeleted: {
+//     type: Boolean,
+//     default: false,
+//   },
+// });
 
-module.exports = mongoose.model("TierLevels", TierSchema);
+// module.exports = mongoose.model("TierLevels", TierSchema);
